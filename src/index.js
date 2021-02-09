4ee07b16d6b3cf6bf9e43530717573855de19c41
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-02-09 21:43:03 +0200

    feat: implemented all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+      }  
+      let min = array[0];
+      array.forEach((e)=> {
+        if (min > e) {
+          min = e;
+        }
+      });
+      return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+      }  
+      let max = array[0];
+      array.forEach((e)=> {
+        if (max < e) {
+          max = e;
+        }
+      });
+      return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+      }  
+      let sum = 0;
+      array.forEach((e)=> {
+        sum += e
+      });
+      return sum/array.length;
 }
